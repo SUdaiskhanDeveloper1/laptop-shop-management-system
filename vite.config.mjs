f1fcb6ec0d405fa1803eb820f6c15a34f0b852c0
Sudaiskhan <engineersudais786@gmail.com>
2025-10-20 17:30:32 +0500

    fix: convert vite config to ESM for vercel

---
+++ vite.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react'),
+      'react-dom': path.resolve('./node_modules/react-dom'),
+    },
+  },
+  server: {
+    port: 5173,
+  },
+  build: {
+
+    chunkSizeWarningLimit: 2000, 
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('antd')) return 'vendor-antd'
+            if (id.includes('firebase')) return 'vendor-firebase'
+            if (id.includes('react')) return 'vendor-react'
+            return 'vendor' 
+          }
+        },
+      },
+    },
+  },
+})
