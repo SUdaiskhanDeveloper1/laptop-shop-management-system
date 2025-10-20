dadf91ddcd94e99d36fba36cd3c13b50db91b4b5
Sudaiskhan <engineersudais786@gmail.com>
2025-10-20 15:57:47 +0500

    final one

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-    },
-  },
-  server: {
-    port: 5173,
-  },
 })
