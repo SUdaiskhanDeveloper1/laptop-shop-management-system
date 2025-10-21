6ff4c2175cd2f40a5c103245ea39ed75e0dc95ce
Sudaiskhan <engineersudais786@gmail.com>
2025-10-21 11:01:11 +0500

    changes done

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      react: path.resolve('./node_modules/react'),
-      'react-dom': path.resolve('./node_modules/react-dom'),
-    },
-  },
   define: {
-    'process.env': {}, // ✅ Fix for AntD + Firebase
+    'process.env': {}, 
   },
   server: {
     port: 5173,
